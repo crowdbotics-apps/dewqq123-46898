9d1e350938aa1f7331126ad921eeed38d5d19255
Crowdbotics <team@crowdbotics.com>
2024-03-08 22:21:55 +0000

    Initial commit

---
+++ backend/dewqq123_46898/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for dewqq123_46898 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "dewqq123_46898.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "dewqq123-46898",
+  url: "https://dewqq123-46898.botics.co",
+  api: "https://dewqq123-46898.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
